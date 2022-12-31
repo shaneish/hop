60f0b7edc59477b4f6043ef8b6210e774b648604
shane stephenson (shalst) <stephenson.shane.a@gmail.com>
2022-12-31 16:21:01 -0500

    added stdout statements showing hop added

--- src/hopper.rs
+++ src/hopper.rs
@@ -9,7 +9,12 @@ use dirs::home_dir;
 
 pub fn add_hop<T: AsRef<Path>>(path: T, name: &str) -> io::Result<()> {
     let hop_config_dir = format!("{}/.config/hop", home_dir().unwrap().into_os_string().into_string().unwrap());
-    symlink::symlink_dir(path.as_ref(), format!("{}/{}", hop_config_dir, name))
+    let sym_result = symlink::symlink_dir(path.as_ref(), format!("{}/{}", hop_config_dir, name));
+    match sym_result {
+        Ok(_) => println!("[hop] {} -> {}", name, path.as_ref().display().to_string()),
+        Err(_) => println!("[error] unable to add hop {} -> {}", name, path.as_ref().display().to_string()),
+    };
+    sym_result
 }
 
 pub fn hop(name: &str) -> io::Result<()> {
