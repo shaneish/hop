4ddf10b4323a1f287be9ca84259c314ee76ff574
Shane Stephenson <stephenson.shane.a@gmail.com>
2023-02-09 00:33:16 -0500

    updated Makefile to give option to use python install script

--- runners/add_runners_windows_gitbash.sh
+++ runners/add_runners_windows_gitbash.sh
@@ -4,7 +4,7 @@ export hopper_loc=$PWD/target/release/bhop
 cat $sh_loc > ./temp_sh
 sed -i "s|__HOPPERCMD__|$hopper_loc|g" ./temp_sh
 
-# add runner function to .zshrc if needed
+# add runner function to .bash_profile if needed
 if [ -f ~/.bash_profile ] && ! grep -q "hp()" ~/.bash_profile; then
     echo "[info] adding git-bash runner..."
     cat ./temp_sh >> ~/.bash_profile
