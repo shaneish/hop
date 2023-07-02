7f64fd7f6fd7ab0ed7378b55f8890c3b03c21705
Shane Stephenson <stephenson.shane.a@gmail.com>
2023-07-02 02:17:24 -0400

    fixed powershell runner

--- runners/scripts/runner.ps1
+++ runners/scripts/runner.ps1
@@ -1,6 +1,6 @@
 function __FUNCTION_ALIAS__($cmd, $p1, $p2, $p3, $p4) {
     $fullCmd = Invoke-Expression "__HOPPERCMD__ $cmd $p1 $p2 $p3 $p4"
-    if ($fullCmd -contains "__CMD_SEPARATOR__") {
+    if ($fullCmd.Contains("__CMD_SEPARATOR__")) {
         $to_move, $to_exec = $fullCmd.trim().Split("__CMD_SEPARATOR__", 2)
         cd $to_move
         Invoke-Expression $to_exec
